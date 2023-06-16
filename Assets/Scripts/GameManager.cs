6029a7be6e3afc00c64b14c81acef555a76389bd
Grigori89 <128844211+Grigori89@users.noreply.github.com>
2023-06-16 15:32:51 +0200

    MultimediaZad03

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject endPanel;
+    private GameObject[] allBoxes;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    private void Update()
+    {
+        allBoxes = GameObject.FindGameObjectsWithTag("Box");
+        if(allBoxes.Length == 0)
+        {
+            endPanel.SetActive(true);
+
+        }
+    }
+
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
